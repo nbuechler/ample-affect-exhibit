b6675589bd841c846feda5dbf047921d1aa39bf1
nbuechler <natebuechler@gmail.com>
2016-09-05 23:24:24 -0400

    working form that makes api POST calls to speed affect scorer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,8 +1,7 @@
 import fetch from 'isomorphic-fetch';
 
-export const REQUEST_DATA = 'REQUEST_DATA';
-export const RECEIVE_DATA = 'RECEIVE_DATA';
 
+export const REQUEST_DATA = 'REQUEST_DATA';
 function requestData(dataset) {
   return {
     type: REQUEST_DATA,
@@ -10,28 +9,45 @@ function requestData(dataset) {
   };
 }
 
+export const RECEIVE_DATA = 'RECEIVE_DATA';
 function receiveData(dataset, json) {
+
+  let data = json
+  if (dataset !== 'nlp') {
+    data = json.all
+  }
+
   return {
     type: RECEIVE_DATA,
     dataset,
-    data: json.all,
+    data: data,
     receivedAt: Date.now()
   };
 }
 
-function fetchData(dataset) {
+export const SUBMIT_REQUEST = 'SUBMIT_REQUEST';
+function submitRequest(values) {
+  return {
+    type: SUBMIT_REQUEST,
+    values,
+    receivedAt: Date.now()
+  };
+}
+
+function fetchData(dataset, port) {
   return dispatch => {
     dispatch(requestData(dataset));
     // return fetch(`http://www.user.com/r/${user}.json`)
     // return fetch(`http://127.0.0.1:5000/${user}`)
 
-    var options = {
+    let options = {
       credentials: localStorage.getItem('credentials') | 'none'
     }
 
-    var ip = window.location.hostname;
+    let ip = window.location.hostname;
+    let portNum = port || 3000
 
-    return fetch(`http://` + ip + `:3000/${dataset}/?credentials=` + options.credentials)
+    return fetch(`http://` + ip + `:` + portNum + `/${dataset}/?credentials=` + options.credentials)
       .then(req => req.json())
       .then(json => dispatch(receiveData(dataset, json)));
   };
@@ -54,4 +70,30 @@ export function fetchDataIfNeeded(dataset) {
       return dispatch(fetchData(dataset));
     }
   };
+}
+
+export function nlpSubmit(data) {
+  return dispatch => {
+    dispatch(submitRequest(data))
+
+    let ip = window.location.hostname;
+
+    // TODO: Change 'emotion_ml' to a paramater that is extacted from the data!
+    let url = `http://` + ip + `:5000/helpers/analyze_emotion_set/emotion_ml/`
+
+    fetch(url, {
+      // credentials: 'include', //pass cookies, for authentication
+      method: 'POST',
+      mode: 'CORS',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(json => dispatch(receiveData('nlp', json)))
+    .catch(err => console.log(err));
+
+  }
 }
