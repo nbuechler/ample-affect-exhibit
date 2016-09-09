dd7b09f53efaaa8d1078530e910a7745752dcc4d
nbuechler <natebuechler@gmail.com>
2016-09-08 23:23:41 -0400

    added some sorting, but its buggy, even though there is a nice table now

--- src/components/tables/NLPTable.js
+++ src/components/tables/NLPTable.js
@@ -16,9 +16,25 @@ class NLPTable extends Component {
 
     if (this.props.data.length > 0) {
       primaryArea = [];
+      secondaryArea = [];
       let array = this.props.data[0].emotion_set
+      let sortedArray = this.props.data[0].emotion_set.sort(function(a,b) {
+                            return a.r_affect_score - b.r_affect_score;
+                        });
+      let top5 = sortedArray.splice(0,5)
+      for (var i = 0; i < top5.length; i++) {
+        primaryArea.push(
+          <tr>
+            <td key={i + '-r-affect'}>{array[i].emotion}</td><td key={i + '-score'}>{array[i].r_affect_score} </td>
+          </tr>
+        )
+      }
       for (var i = 0; i < array.length; i++) {
-        primaryArea.push(<p key={i}>{array[i].emotion},{array[i].r_affect_score} </p>)
+        secondaryArea.push(
+          <tr>
+            <td key={i + '-r-affect'}>{array[i].emotion}</td><td key={i + '-score'}>{array[i].r_affect_score} </td>
+          </tr>
+        )
       }
     }
     return (
@@ -40,7 +56,19 @@ class NLPTable extends Component {
         {data.length > 0 &&
           <div style={{ opacity: isFetching ? 0.5 : 1 }}>
             <div style={{ textAlign: 'center' }}>
-              {primaryArea}
+              <Alert>The top five emotions from emotion list is below, sorted, based on score</Alert>
+              <Table style={{margin: 'auto', textAlign: 'center'}} striped bordered condensed hover>
+                <tbody>
+                {primaryArea}
+                </tbody>
+              </Table>
+              <hr></hr>
+              <Alert>The full emotion list is below, sorted, from A-Z</Alert>
+              <Table style={{margin: 'auto', textAlign: 'center'}} striped bordered condensed hover>
+                <tbody>
+                {secondaryArea}
+                </tbody>
+              </Table>
             </div>
           </div>
         }
