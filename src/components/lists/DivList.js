eb8d277eed41d117af6585c68e05c33a186f9485
nbuechler <natebuechler@gmail.com>
2016-09-19 22:29:15 -0400

    removed a useless if statement

--- src/components/lists/DivList.js
+++ src/components/lists/DivList.js
@@ -10,20 +10,18 @@ export default class DivList extends React.Component {
   render () {
     let fdist = this.props.fdist,
         listItems = [];
-    if (true) {
-      for (var i = 0; i < fdist.length; i++) {
-        listItems.push(
-          <h6 key={'list-item-' + fdist[i]}>
-            <span className="pull-left">
-              {fdist[i][0]}
-            </span>
-            <span className="pull-right">
-              {fdist[i][1]}
-            </span>
-            <br></br>
-          </h6>
-        )
-      }
+    for (var i = 0; i < fdist.length; i++) {
+      listItems.push(
+        <h6 key={'list-item-' + fdist[i]}>
+          <span className="pull-left">
+            {fdist[i][0]}
+          </span>
+          <span className="pull-right">
+            {fdist[i][1]}
+          </span>
+          <br></br>
+        </h6>
+      )
     }
     if (listItems.length < 1) {
       listItems.push(
