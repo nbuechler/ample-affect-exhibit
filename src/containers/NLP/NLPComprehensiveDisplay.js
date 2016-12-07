95ff22d223c16078af9188dc2ad6c7cbed70019f
nbuechler <natebuechler@gmail.com>
2016-12-06 23:56:09 -0500

    added comprehensive work in progress section

---
+++ src/containers/NLP/NLPComprehensiveDisplay.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+import { Table, Button, Row, Col, Panel, Accordion } from 'react-bootstrap';
+
+import NLPComprehensiveForm from '../../components/forms/NLPComprehensiveForm'
+import NLPTable from '../../components/tables/NLPTable'
+import NLPRankTable from '../../components/tables/NLPRankTable'
+import NLPLaggerTable from '../../components/tables/NLPLaggerTable'
+
+class NLPDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.handleRefreshClick = this.handleRefreshClick.bind(this);
+  }
+
+  handleRefreshClick(e) {
+    e.preventDefault();
+  }
+
+  render () {
+    const { } = this.props;
+
+    return (
+      <div>
+        <a href="#/dashboard"><Button className="pull-right">Back</Button></a>
+        <div style={{paddingBottom: '100px'}}>
+          <h1>Natural Language Processing</h1>
+          <hr></hr>
+          <Accordion>
+            <Panel header="Collapsible Group Item #1" eventKey="1">
+              <Row>
+                <Col lg={12}>
+                  <Panel>
+                    <NLPComprehensiveForm/>
+                  </Panel>
+                </Col>
+              </Row>
+            </Panel>
+            <Panel header="Collapsible Group Item #2" eventKey="2">
+              <Row>
+                <Col lg={3}>
+                  <Panel>
+                    <NLPRankTable/>
+                  </Panel>
+                  <Panel>
+                    <NLPLaggerTable/>
+                  </Panel>
+                </Col>
+              </Row>
+            </Panel>
+            <Panel header="Collapsible Group Item #3" eventKey="3">
+              <Row>
+                <Col lg={9}>
+                  <Panel>
+                    <NLPTable/>
+                  </Panel>
+                </Col>
+              </Row>
+            </Panel>
+          </Accordion>
+        </div>
+      </div>
+    );
+  }
+}
+
+NLPDisplay.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  const { } = state;
+
+  return {
+  };
+}
+
+export default connect(mapStateToProps)(NLPDisplay);
