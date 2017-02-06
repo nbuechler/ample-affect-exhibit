a62aa8d1bd198e7ddf72cc1c85e7d696ec13f0c5
nbuechler <natebuechler@gmail.com>
2017-02-05 23:43:44 -0500

    feat: Add word count lists of each POS

--- src/components/tables/NLPDetailFreqDistTableModule.js
+++ src/components/tables/NLPDetailFreqDistTableModule.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import DivListGroup from '../groups/DivListGroup'
-import DivList from '../lists/DivList'
-import StatisticGroup from '../groups/StatisticGroup'
-import AffectCorpusLengthRowTableGroup from '../groups/AffectCorpusLengthRowTableGroup'
-import AffectNormalizedScoreRowTableGroup from '../groups/AffectNormalizedScoreRowTableGroup'
+import POSCountDivList from '../lists/POSCountDivList'
 
 import { Table, Alert, Button } from 'react-bootstrap';
 
@@ -52,76 +48,76 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
     function handlePOS(datapoint) {
       switch (datapoint['pos']) {
         case 'IN': //Preposition
-          return 0
+          return [0,0]
           break;
         case 'CC': //Preposition
-          return 0
+          return [0,1]
           break;
         case 'RP': //Particle
-          return 0
+          return [0,2]
           break;
         case 'PRP': //Nouns
-          return 1
+          return [1,3]
           break;
         case 'PRP$': //Nouns
-          return 1
+          return [1,4]
           break;
         case 'NN': //Nouns
-          return 1
+          return [1,5]
           break;
         case 'NNP': //Nouns
-          return 1
+          return [1,6]
           break;
         case 'NNS': //Nouns
-          return 1
+          return [1,7]
           break;
         case 'JJ': //Adjectives
-          return 2
+          return [2,8]
           break;
         case 'JJR': //Adjectives
-          return 2
+          return [2,9]
           break;
         case 'JJS': //Adjectives
-          return 2
+          return [2,10]
           break;
         case 'RB': //Adjectives
-          return 2
+          return [2,11]
           break;
         case 'RBR': //Adjectives
-          return 2
+          return [2,12]
           break;
         case 'RBS': //Adjectives
-          return 2
+          return [2,13]
           break;
         case 'MD': //Verbs
-          return 3
+          return [3,14]
           break;
         case 'VB': //Verbs
-          return 3
+          return [3,15]
           break;
         case 'VBD': //Verbs
-          return 3
+          return [3,16]
           break;
         case 'VBG': //Verbs
-          return 3
+          return [3,17]
           break;
         case 'VBN': //Verbs
-          return 3
+          return [3,18]
           break;
         case 'VBP': //Verbs
-          return 3
+          return [3,19]
           break;
         case 'VBZ': //Verbs
-          return 3
+          return [3,20]
           break;
         case 'CD': //Number
-          return 4
+          return [4,21]
           break;
         case 'FW': //Foreign word
-          return 4
+          return [4,22]
           break;
         case 'DT': //Determiner word
-          return 4
+          return [4,23]
           break;
         default:
           return 'infinity'
@@ -132,8 +128,13 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
     let rawData = allBinData,
         processedData = [],
         posData = [0, 0, 0, 0, 0],
+        posExtendedData = [],
         datapoint = {},
         counter = 0;
+    // Create a vector of 0's for posExtendedData
+    for (var i = 0; i < 24; i++) {
+      posExtendedData[i] = 0
+    }
     for (let i of rawData) {
       for (let j of i) {
         datapoint = {
@@ -144,28 +145,14 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
         }
         processedData.push(datapoint)
         let dataArrayPosition = handlePOS(datapoint);
-        switch (dataArrayPosition) {
-          case 0:
-              posData[dataArrayPosition] += 1
-            break;
-          case 1:
-              posData[dataArrayPosition] += 1
-            break;
-          case 2:
-              posData[dataArrayPosition] += 1
-            break;
-          case 3:
-              posData[dataArrayPosition] += 1
-            break;
-          case 4:
-              posData[dataArrayPosition] += 1
-            break;
-          default:
-            console.log('error: ' + dataArrayPosition);
-            console.log(datapoint);
-            break;
-
-        }
+        if (dataArrayPosition[0] >= 0) {
+          posData[dataArrayPosition[0]] += 1
+          posExtendedData[dataArrayPosition[1]] += 1
+        } else {
+          console.log('error: ' + dataArrayPosition);
+          console.log(datapoint);
+          break;
+        };
       }
       counter++
     }
@@ -175,7 +162,7 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
     let totalWords = posData.reduce((a, b) => a + b, 0);
 
     return (
-      <div>
+      <div className="radiant--graph-wrapper">
         <Table condensed key={'table'} style={{
             fontSize: '12px',
             textAlign: 'left',
@@ -185,31 +172,31 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
           }}>
           <thead>
             <tr>
-              <th>
+              <th className="radiant--key-cell_heading-wrapper">
                 <div className="radiant--key-cell_heading">
                   <div className="radiant--key-cell_color-swatch here radiant--key-cell_color-swatch_prep"></div>
                   Preposition ({posData[0]})
                 </div>
               </th>
-              <th>
+              <th className="radiant--key-cell_heading-wrapper">
                 <div className="radiant--key-cell_heading">
                   <div className="radiant--key-cell_color-swatch radiant--key-cell_color-swatch_noun"></div>
                   Noun ({posData[1]})
                 </div>
               </th>
-              <th>
+              <th className="radiant--key-cell_heading-wrapper">
                 <div className="radiant--key-cell_heading">
                   <div className="radiant--key-cell_color-swatch radiant--key-cell_color-swatch_adj"></div>
                   Adjective ({posData[2]})
                 </div>
               </th>
-              <th>
+              <th className="radiant--key-cell_heading-wrapper">
                 <div className="radiant--key-cell_heading">
                   <div className="radiant--key-cell_color-swatch radiant--key-cell_color-swatch_verb"></div>
                   Verb ({posData[3]})
                 </div>
               </th>
-              <th>
+              <th className="radiant--key-cell_heading-wrapper">
                 <div className="radiant--key-cell_heading">
                   <div className="radiant--key-cell_color-swatch radiant--key-cell_color-swatch_other"></div>
                   Other ({posData[4]})
@@ -218,13 +205,7 @@ export default class NLPDetailFreqDistTableModule extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            <POSCountDivList posExtendedData={posExtendedData}></POSCountDivList>
           </tbody>
         </Table>
       </div>
