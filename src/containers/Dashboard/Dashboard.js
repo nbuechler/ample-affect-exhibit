f75ee5e44f89cdab5ce3107e8e77d977744e5653
nbuechler <natebuechler@gmail.com>
2017-02-08 23:58:11 -0500

    feat: Modify overview page

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { Table } from 'react-bootstrap';
+import { Table, Button, Panel, Row, Col } from 'react-bootstrap';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -13,14 +13,41 @@ class Dashboard extends Component {
     const notImplemented = {textDecoration: "line-through", color: "gray"}
     return (
       <div>
-        <h1><i className="fa fa-tachometer" aria-hidden="true"></i> Dashboard</h1>
-        <p>Choose an option:</p>
+        <h1><i className="fa fa-globe" aria-hidden="true"></i> Overview</h1>
+        <p>View linguitic processes from prior analyses below:</p>
         <br></br>
-        <div><i className="fa fa-language" aria-hidden="true"></i> <a href="#/nlp">Natural Language Processing</a></div>
-        <hr></hr>
-        <div><i className="fa fa-camera" aria-hidden="true"></i> <span style={notImplemented}>Computer Vision</span> (Not Implemented)</div>
-        <div><i className="fa fa-flash" aria-hidden="true"></i> <span style={notImplemented}>Skin Conductance</span> (Not Implemented)</div>
-        <div><i className="fa fa-thermometer-full" aria-hidden="true"></i> <span style={notImplemented}>Body Temperature</span> (Not Implemented)</div>
+        <Row>
+          <Col lg={9}>
+            <div>
+              <Panel header={'Most recent processes'}>
+                Your most recent processes are shown here. Right now you don't have any! Why not run one?
+              </Panel>
+            </div>
+          </Col>
+          <Col lg={3}>
+            <div>
+              <Panel header={'Begin a new process'}>
+                <div style={{marginBottom: '10px'}}>
+                  For when you need your results right now.
+                </div>
+                <div style={{textAlign: 'right'}}>
+                  <Button style={{width: '200px'}} bsSize="small" href="#/nlp">
+                    <i className="fa fa-fire" aria-hidden="true"></i> Fast Processing
+                  </Button>
+                </div>
+                <hr></hr>
+                <div style={{marginBottom: '10px'}}>
+                  For detailed results, that are the most precise.
+                </div>
+                <div style={{textAlign: 'right'}}>
+                  <Button style={{width: '200px'}} bsSize="small" href="#/nlp">
+                    <i className="fa fa fa-tint" aria-hidden="true"></i> Precise Processing
+                  </Button>
+                </div>
+              </Panel>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
   }
