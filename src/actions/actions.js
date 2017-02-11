fd1f6a002d629c7519feab403d146b7011eaf66a
nbuechler <natebuechler@gmail.com>
2017-02-10 22:22:28 -0500

    feat: Begin integrating ample-to-scorer path

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -88,7 +88,8 @@ export function nlpSubmit(data) {
     let ip = window.location.hostname;
 
     // TODO: Change 'emotion_ml' to a paramater that is extacted from the data!
-    let url = `http://` + ip + `:5000/helpers/analyze_emotion_set/` + data.emotion_set +`/`
+    // let url = `http://` + ip + `:5000/helpers/analyze_emotion_set/` + data.emotion_set +`/`
+    let url = `http://` + ip + `:3000/analyzeEmotionSet/` // + data.emotion_set +`/`
 
     fetch(url, {
       // credentials: 'include', //pass cookies, for authentication
