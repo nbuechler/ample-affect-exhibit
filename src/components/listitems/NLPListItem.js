16d774f9ee98421a3759dabc1f367c6a79b1c614
nbuechler <natebuechler@gmail.com>
2017-02-27 23:39:16 -0500

    feat: Improve the layout of the processes

--- src/components/listitems/NLPListItem.js
+++ src/components/listitems/NLPListItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Alert, Col, ListGroupItem, Button } from 'react-bootstrap';
+import { Alert, Col, ListGroupItem, Button, Well } from 'react-bootstrap';
 
 export default class NLPListItem extends React.Component {
   constructor (props) {
@@ -11,14 +11,25 @@ export default class NLPListItem extends React.Component {
     let data = this.props.data
     return (
       <ListGroupItem>
-        <div>
-          {data.doc}
-        </div>
-        <div>
-          {data.name}
-        </div>
-        <div>
-          {data.date[0]}
+        <div style={{fontSize: "12px"}}>
+          <div className="pull-left">
+            <div>
+              Emotion Set: {data.name}
+            </div>
+          </div>
+          <div className="pull-right">
+            <div>
+              Date: {data.date[0]}
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <div>
+            <div style={{fontWeight: "900"}}>Document</div>
+            <Well>
+              {data.doc}
+            </Well>
+          </div>
         </div>
         <div style={{textAlign: "right"}}>
           <Button style={{width: '200px'}} bsSize="xsmall" href="#/nlp">
